e8deca54a5416829baa11708c52010f54e948b70
Bishnu <thapabishnu20@gmail.com>
2022-08-15 11:35:43 +0545

    added search functionality

--- cars/urls.py
+++ cars/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
-    path('nepal/', views.Nepal, name='nepal'),
     path('<slug:car_slug>/', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,4 @@
+from calendar import c
 from django.shortcuts import render, get_object_or_404
 
 from .models import Car
@@ -18,13 +19,23 @@ def cars(request):
     return render(request, 'cars/cars.html', context)
 
 
-def Nepal(request):
-    return render(request, 'cars/nepal.html')
-
-
 def car_detail(request, car_slug):
     single_car = get_object_or_404(Car, slug=car_slug)
     context = {
         'single_car': single_car
     }
     return render(request, 'cars/car_detail.html', context)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            cars = Car.objects.filter(description__icontains=keyword)
+
+    context = {
+        'cars': cars
+    }
+    return render(request, 'cars/search.html', context)
