af34b061a9980b54ea89628fabb909a928ac64b0
Bishnu <thapabishnu20@gmail.com>
2022-08-15 14:24:54 +0545

    admin customization done

--- cars/views.py
+++ cars/views.py
@@ -9,12 +9,21 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 def cars(request):
     cars = Car.objects.order_by('-created_date')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list(
+        'body_style', flat=True).distinct()
     paginator = Paginator(cars, 4)  # 3 items to display per page
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
     context = {
         # 'cars': cars,
-        'cars': paged_cars
+        'cars': paged_cars,
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search,
 
     }
     return render(request, 'cars/cars.html', context)
