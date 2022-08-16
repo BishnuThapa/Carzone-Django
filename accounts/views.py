5b731ede04202d99965d41edf1d6c4c96b95bee5
Bishnu <thapabishnu20@gmail.com>
2022-08-16 09:53:11 +0545

    login registration & dashboard page setup

---
+++ accounts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        print("Hello world")
+    return render(request, 'accounts/register.html')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+def logout(request):
+    return redirect('home')
