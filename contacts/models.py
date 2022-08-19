f3fa584bbaddc7977819c794711a72e98ad830d2
Bishnu <thapabishnu20@gmail.com>
2022-08-19 20:54:07 +0545

    send message error fixing

--- contacts/models.py
+++ contacts/models.py
@@ -7,7 +7,8 @@ from django.db import models
 class Contact(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    car_id = models.IntegerField()
+    # car_id = models.IntegerField()
+    car_slug = models.SlugField(max_length=255, null=True, blank=True)
     customer_need = models.CharField(max_length=100)
     car_title = models.CharField(max_length=100)
     city = models.CharField(max_length=100)
--- contacts/views.py
+++ contacts/views.py
@@ -7,7 +7,7 @@ from .models import Contact
 
 def inquiry(request):
     if request.method == "POST":
-        car_id = request.POST['car_id']
+        car_slug = request.POST['car_slug']
         car_title = request.POST['car_title']
         user_id = request.POST['user_id']
         first_name = request.POST['first_name']
@@ -19,10 +19,18 @@ def inquiry(request):
         phone = request.POST['phone']
         message = request.POST['message']
 
-        contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id, first_name=first_name, last_name=last_name,
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contacted = Contact.objects.all().filter(car_slug=car_slug, user_id=user_id)
+            if has_contacted:
+                messages.error(
+                    request, 'You have already made an inquiry about this car. Please wait until we get back to you.')
+                return redirect('/cars/', car_slug)
+
+        contact = Contact(car_slug=car_slug, car_title=car_title, user_id=user_id, first_name=first_name, last_name=last_name,
                           customer_need=customer_need, city=city, state=state, email=email, phone=phone, message=message)
         contact.save()
         messages.success(
             request, 'Your request has been submitted, we will get back you shortly.')
 
-        return redirect('/cars/'+car_id)
+        return redirect('/cars/'+car_slug)
